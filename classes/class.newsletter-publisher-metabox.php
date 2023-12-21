d36cf998aa73e3fd7075c9b4682cd923439206bf
raselsha <raselsha@gmail.com>
2023-12-21 21:00:54 +0600

    nonce and security check

--- classes/class.newsletter-publisher-metabox.php
+++ classes/class.newsletter-publisher-metabox.php
@@ -31,11 +31,11 @@ if( ! class_exists('Newsletter_Publisher_Metabox') ){
             global $typenow;
             if( $typenow == 'newsletter' ) {
                 wp_enqueue_media();
-                wp_enqueue_script('meta-box-image',NEWSLETTER_PUB_URL.'assets/js/media.js',array( 'jquery' ),time(),true);
+                wp_register_script('meta-box-image',NEWSLETTER_PUB_URL.'assets/js/media.js',array( 'jquery' ),time(),true);
                 wp_localize_script( 'meta-box-image', 'meta_image',
                     array(
-                        'title' => __( 'Choose or Upload Media', 'newsletter-publisher' ),
-                        'button' => __( 'Use this media', 'newsletter-publisher' ),
+                        'title' => __( 'Choose or Upload Media', TEXTDOMAIN ),
+                        'button' => __( 'Use this media', TEXTDOMAIN ),
                     )
                 );
                 wp_enqueue_script( 'meta-box-image' );
@@ -43,6 +43,26 @@ if( ! class_exists('Newsletter_Publisher_Metabox') ){
         }
 
         public function save_post($post_id){
+
+            if( isset( $_POST['newsletter_nonce'] ) ){
+                if( ! wp_verify_nonce( $_POST['newsletter_nonce'], 'newsletter_nonce' ) ){
+                    return;
+                }
+            }
+
+            if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ){
+                return;
+            }
+
+            if( isset($_POST['post_type']) && $_POST['post_type'] === 'newsletter' ){
+                if( ! current_user_can('edit_page',$post_id) ){
+                    return;
+                }
+                elseif( ! current_user_can('edit_post',$post_id) ){
+                    return;
+                }
+            }   
+
             if( isset($_POST['action']) and $_POST['action']=='editpost' ){
                 $old_data = get_post_meta($post_id,'newsletter_file',true);
                 $new_data = $_POST['newsletter_file'];
--- views/view.metabox.php
+++ views/view.metabox.php
@@ -3,12 +3,13 @@ $meta = get_post_meta($post->ID,'newsletter_file',true);
 ?>
 
 <table  cellspacing="5">
+	<input type="hidden" name="newsletter_nonce" value="<?php echo wp_create_nonce("newsletter_nonce"); ?>">
 	<tr>
 		<th>
 			<label for="newsletter_file"><?php _e( 'Field Label', 'events' )?></label>
 		</th>
 		<td>
-			<input type="url" class="form-control border" name="newsletter_file" id="newsletter_file" value="<?php esc_html_e($meta);  ?>" required>
+			<input type="url" class="form-control border" name="newsletter_file" id="newsletter_file" value="<?php echo ( isset($meta) ) ? esc_url($meta) : '' ;  ?>" required>
 			<button type="button" class="button" id="events_video_upload_btn" data-media-uploader-target="#newsletter_file"><?php _e( 'Upload Media', 'myplugin' )?></button>
 		</td>
 	</tr>
