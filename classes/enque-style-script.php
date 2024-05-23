1b25005638faa6be8279c91acb087c8a4af02652
Shahadat Hossain <raselsa@gmail.com>
2024-05-23 21:41:57 +0600

    fix: enqueue version added for main version

--- classes/enque-style-script.php
+++ classes/enque-style-script.php
@@ -11,24 +11,22 @@ if( ! class_exists('PDF_Emd_Vwr_Enque') ){
         {
             add_action('admin_enqueue_scripts',[$this,'backend_style']);
             add_action('wp_enqueue_scripts',[$this,'frontend_style']);
-           
         }
 
         public function backend_style(){
             wp_enqueue_style( 'font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css','','5.9.0');
-            wp_enqueue_style('main',PDF_Emd_Vwr_URL.'assets/css/admin.css',['wp-color-picker','font-awesome'],'1.0','all');
+            wp_enqueue_style('main',PDF_Emd_Vwr_URL.'assets/css/admin.css',['wp-color-picker','font-awesome'],PDF_Emd_Vwr_VERSION,'all');
             wp_enqueue_media();        
             wp_enqueue_script( 'jquery-ui', 'https://code.jquery.com/ui/1.13.3/jquery-ui.min.js','','1.13.3',true);
-            wp_enqueue_script( 'pdf-embed-viewer', PDF_Emd_Vwr_URL.'assets/js/admin.js', ['wp-color-picker','jquery-ui'], '1.0', true );
-                    
+            wp_enqueue_script( 'pdf-embed-viewer', PDF_Emd_Vwr_URL.'assets/js/admin.js', ['wp-color-picker','jquery-ui'], PDF_Emd_Vwr_VERSION, true );
         }
 
         public function frontend_style(){
 
             if(is_singular( 'pdf-embed-viewer') || is_post_type_archive( 'pdf-embed-viewer') ){
-                wp_enqueue_style('pdf-frontend-style',PDF_Emd_Vwr_URL.'assets/css/frontend.css',[],'1.0','all');
+                wp_enqueue_style('pdf-frontend-style',PDF_Emd_Vwr_URL.'assets/css/frontend.css',[],PDF_Emd_Vwr_VERSION,'all');
                 wp_enqueue_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jqueryexample.min.js','','1.7.2',true);
-                wp_enqueue_script( 'pdf-frontend-script', PDF_Emd_Vwr_URL.'assets/js/frontend.js',['jquery'],'1.0',true);
+                wp_enqueue_script( 'pdf-frontend-script', PDF_Emd_Vwr_URL.'assets/js/frontend.js',['jquery'],PDF_Emd_Vwr_VERSION,true);
 
                 $colors         = get_option('pdf_emd_vwr_opt_colors');         
                 $primary        = $colors['primary'] ? $colors['primary'] : '#c79f62';
