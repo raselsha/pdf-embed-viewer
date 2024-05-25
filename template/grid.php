335bde2a7879859a1bee536df24682b4b727ea77
Shahadat Hossain <raselsa@gmail.com>
2024-05-25 21:12:41 +0600

    feat: grid style

--- template/grid.php
+++ template/grid.php
@@ -39,66 +39,42 @@ if ( wp_is_block_theme() ) {  ?>
 	<?php
 		$archive_title = get_option('pdf_emd_vwr_opt_archive_title');
 		$check_download_archive  =  get_option('pdf_emd_vwr_opt_archive_download');
-		$years =  PDF_Emd_Vwr_CPT::get_posts_years_array('pdf-embed-viewer');
-		if($years):
+		$pdf_emd_vwr_file_url=get_post_meta( get_the_ID(), 'pdf_emd_vwr_file_url', true );
 	?>
 	<h2><?php echo isset($archive_title) ? esc_html($archive_title) : esc_html(the_archive_title()); ?></h2>
-	<div class="archive">
-		<ul class="tabs">
-			<?php 
-				foreach($years as $year): ?>
-					<li class="tab <?php echo esc_attr($year==gmdate('Y')?'active':''); ?>" data-tab-target="#year-<?php echo esc_attr($year);?>" ><?php echo esc_attr($year);?></li>
-			<?php endforeach; ?>
-		</ul>
-		<div class="tabs-content" >
-			<?php foreach($years as $year): ?>
-				<table  class="<?php echo esc_attr($year==gmdate('Y')?'active':''); ?>" data-tab-content  id="year-<?php echo esc_attr($year);?>">
-					<tr>         					    			
-						<th><?php echo esc_html('Month','pdf-embed-viewer') ?></th>
-						<th><?php echo esc_html('Title','pdf-embed-viewer') ?></th>
-						<?php if($check_download_archive == 'yes'): ?>
-						<th><?php echo esc_html('Download','pdf-embed-viewer') ?></th>
-						<?php endif; ?>
-					</tr>
-					<?php
-						$args = array(
-						'post_type'=>'pdf-embed-viewer',
-						'order' => 'ASC',
-						'posts_per_page'=> get_option( 'posts_per_page' ),
-							'date_query' => array(
-								array(
-									'year'  => $year,
-								),
-							),
-					);
-					$WpQuery = new WP_Query($args);    
-						while ( $WpQuery->have_posts() ) :
-							$WpQuery->the_post();
-							?>
-							<tr>
-								<td width="10%"><?php the_time('F'); ?></td>
-								<td width="10%"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></td>
-								<?php if($check_download_archive == 'yes'): ?>
-									<td width="10%">
-										<?php
-											$pdf_emd_vwr_file_url=get_post_meta( get_the_ID(), 'pdf_emd_vwr_file_url', true );
-											if(isset($pdf_emd_vwr_file_url)):
-										?>
-										<a href="<?php echo esc_attr(get_post_meta( get_the_ID(), 'pdf_emd_vwr_file_url', true ))?>" class="download-btn" download><?php echo esc_html('Download','pdf-embed-viewer'); ?> <img src="<?php echo esc_attr(PDF_Emd_Vwr_URL.'assets/images/download.svg'); ?>" alt="<?php echo esc_html('Download icon','pdf-embed-viewer'); ?>"> </a>
-										<?php endif; ?>
-									</td>
-								<?php endif; ?>
+	<div class="archive-grid-style">
 
-							</tr>
-					<?php endwhile; ?>
+		<?php
+			$args = array(
+			'post_type'=>'pdf-embed-viewer',
+			'order' => 'ASC',
+			'posts_per_page'=> get_option( 'posts_per_page' ),
+				'date_query' => array(
+					array(
+						'year'  => $year,
+					),
+				),
+		);
+		$WpQuery = new WP_Query($args);    
+			while ( $WpQuery->have_posts() ) :
+				$WpQuery->the_post();
+				?>
+				<div class="grid-item">
+					<div class="image"><img src="https://dummyimage.com/640x4:3" alt=""></div>
+					<span class="date"><?php the_time('d-m-Y'); ?></span>
 					
-				</table>
-			<?php endforeach; ?>	
-		</div>
+					<div class="content">
+						<h2><?php the_title();?></a></h2>
+						<div class="action">
+							<a href="<?php the_permalink(); ?>" class="download-btn"><i class="far fa-address-book"></i> <?php echo esc_html('Read','pdf-embed-viewer');?></a>
+							<?php if($check_download_archive == 'yes' and  isset($pdf_emd_vwr_file_url)): ?>
+								<a href="<?php echo esc_attr($pdf_emd_vwr_file_ur); ?>" class="download-btn" download><?php echo esc_html('Download','pdf-embed-viewer'); ?> <span class="download-icon" style="background-image: url(<?php echo esc_attr(PDF_Emd_Vwr_URL.'assets/images/download.svg'); ?>);"></span> </a>
+							<?php endif; ?>
+						</div>
+					</div>
+				</div>
+		<?php endwhile; ?>
 	</div>
-	<?php else: ?>
-		<h2><?php echo esc_html('No data found','pdf-embed-viewer'); ?></h2>
-	<?php endif; ?>
 </div>
 
 <?php wp_link_pages(); ?>
