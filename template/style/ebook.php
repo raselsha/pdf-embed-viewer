d8efb6722d6303b2606426536a57e67da958fa36
Shahadat <raselsha@gmail.com>
2024-12-10 22:04:39 +0600

    fix: ebook responsive

--- template/style/ebook.php
+++ template/style/ebook.php
@@ -25,12 +25,20 @@ if( ! defined('ABSPATH') ) { die( "Don't access directly" ); } ?>
 				$WpQuery->the_post();
 				?>
 				<div class="grid-item">
-					<div class="image">
-						<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail() ?></a>
-					</div>					
+					<a href="<?php the_permalink(); ?>">
+						<div class="image">
+							<?php the_post_thumbnail() ?>
+						</div>	
+					</a>				
 					<div class="content">
-						<h2><a href="<?php the_permalink(); ?>"><?php echo esc_html(mb_strimwidth(get_the_title(), 0, 50, '...'));?></a></h2>
-						<div class="">
+						<h2><a href="<?php the_permalink(); ?>">
+						<?php
+							$title = get_the_title(); 
+							$trimmed_title = wp_html_excerpt($title, 80, '...');
+							echo esc_html($trimmed_title);
+						?>
+						</a></h2>
+						<div class="action">
 							<?php PDFEV_Functions::read_button(); ?>
 							<?php PDFEV_Functions::download_button(); ?>
 						</div>
