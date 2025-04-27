9727980473718b70432b2bc12b63be26643e6e59
Shahadat <raselsha@gmail.com>
2025-04-27 18:52:59 +0600

    feat: pdf book viewed

--- classes/enque-style-script.php
+++ classes/enque-style-script.php
@@ -26,10 +26,30 @@ if( ! class_exists('PDFEV_Enque_Style') ){
         public function frontend_style(){
 
             if(is_singular( 'pdfev_embed_viewer') || is_post_type_archive( 'pdfev_embed_viewer') || shortcode_exists('pdfev_viewer') ){
-                wp_enqueue_style('pdf-frontend-style',PDFEV_Const_URL.'assets/css/frontend.css',[],PDFEV_Const_VERSION,'all');
                 
-                wp_enqueue_script( 'pdf-frontend-script', PDFEV_Const_URL.'assets/js/frontend.js',['jquery'],PDFEV_Const_VERSION,true);
 
+                wp_enqueue_style('3dflipbook-style',PDFEV_Const_URL.'assets/css/3dflipbook-style.css',[],PDFEV_Const_VERSION,'all');
+                wp_enqueue_style('pdf-frontend-style',PDFEV_Const_URL.'assets/css/frontend.css',[],PDFEV_Const_VERSION,'all');
+                // javascript ===
+                wp_enqueue_script( 'pdf-frontend-script', PDFEV_Const_URL.'assets/js/frontend.js',['jquery'],PDFEV_Const_VERSION,true);
+                
+                wp_enqueue_script( 'pdf-three.min', PDFEV_Const_URL.'assets/js/three.min.js',['jquery'],PDFEV_Const_VERSION,true);
+                wp_enqueue_script( 'pdf-min', PDFEV_Const_URL.'assets/js/pdf.min.js',['jquery'],PDFEV_Const_VERSION,true);
+                add_action('wp_footer', function() {
+                    ?>
+                    <script type="text/javascript">
+                        window.PDFJS_LOCALE = {
+                            pdfJsWorker: "<?php echo PDFEV_Const_URL; ?>assets/js/pdf.worker.js",
+                            pdfJsCMapUrl: "https://cdn.3dflipbook.net/cmaps",
+                        };
+                        
+                        </script>
+                    <?php
+                });
+                wp_enqueue_script('jquery');
+                wp_enqueue_script( 'pdf-3dflipbook', PDFEV_Const_URL.'assets/js/3dflipbook.min.js',['pdf-frontend-script'],PDFEV_Const_VERSION,true);
+                wp_enqueue_script( 'pdf-simple-jquery-pdf', PDFEV_Const_URL.'assets/js/simple-jquery-pdf.js',['pdf-frontend-script'],PDFEV_Const_VERSION,true);
+                wp_add_inline_script('pdf-3dflipbook', 'window.$ = window.jQuery;', 'before');
                 $colors         = get_option('pdfev_css_colors');         
                 $primary        = esc_html($colors['primary'] ? $colors['primary'] : '#c79f62');
                 $secondary      = esc_html($colors['secondary'] ? $colors['secondary'] : '#666');
@@ -46,7 +66,8 @@ if( ! class_exists('PDFEV_Enque_Style') ){
 
             wp_localize_script('pdf-frontend-script', 'pdfevAjax', array(
                 'ajaxurl' => admin_url('admin-ajax.php'),
-                'ajaxnonce'   => wp_create_nonce('pdf_ajax_nonce')
+                'ajaxnonce'   => wp_create_nonce('pdf_ajax_nonce'),
+                'pdfevurl' => PDFEV_Const_URL,
             ));
             
         }
