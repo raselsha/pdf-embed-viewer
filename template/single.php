50bfaf68148f687b159ffb81586b5e4e38f3d036
Shahadat Hossain <raselsha@gmail.com>
2026-07-26 13:51:23 +0600

    single template: use block_template_part for block themes, get_header for classic

--- template/single.php
+++ template/single.php
@@ -9,8 +9,38 @@ if ( ! defined('ABSPATH') ) {
     die( "Don't access directly" );
 }
 
-get_header();
+if ( wp_is_block_theme() ) :
 ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div class="wp-site-blocks">
+    <header class="wp-block-template-part">
+        <?php block_template_part('header'); ?>
+    </header>
+
+    <div class="pdfev-embed-viewer">
+        <?php do_action('pdfev_template_single_header'); ?>
+        <?php echo do_shortcode('[pdfev_embed_viewer id="' . get_the_ID() . '"]'); ?>
+        <?php do_action('pdfev_template_single_footer'); ?>
+    </div>
+
+    <footer class="wp-block-template-part">
+        <?php block_template_part('footer'); ?>
+    </footer>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
+
+<?php else : ?>
+
+<?php get_header(); ?>
 
 <div class="pdfev-embed-viewer">
     <?php do_action('pdfev_template_single_header'); ?>
@@ -18,5 +48,5 @@ get_header();
     <?php do_action('pdfev_template_single_footer'); ?>
 </div>
 
-<?php
-get_footer();
+<?php get_footer(); ?>
+<?php endif; ?>
