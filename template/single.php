619cc5494c95e12bb814dd1a8fb163e43bfd06e6
Shahadat Hossain <raselsha@gmail.com>
2026-07-26 13:54:38 +0600

    single template: match block theme structure, add skip-link

--- template/single.php
+++ template/single.php
@@ -19,16 +19,21 @@ if ( wp_is_block_theme() ) :
 </head>
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
+<a class="skip-link screen-reader-text" id="wp-skip-link" href="#wp--skip-link--target">Skip to content</a>
 <div class="wp-site-blocks">
     <header class="wp-block-template-part">
         <?php block_template_part('header'); ?>
     </header>
 
-    <div class="pdfev-embed-viewer">
-        <?php do_action('pdfev_template_single_header'); ?>
-        <?php echo do_shortcode('[pdfev_embed_viewer id="' . get_the_ID() . '"]'); ?>
-        <?php do_action('pdfev_template_single_footer'); ?>
-    </div>
+    <main class="wp-block-group" id="wp--skip-link--target" style="margin-top:var(--wp--preset--spacing--60)">
+        <div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)">
+            <div class="pdfev-embed-viewer">
+                <?php do_action('pdfev_template_single_header'); ?>
+                <?php echo do_shortcode('[pdfev_embed_viewer id="' . get_the_ID() . '"]'); ?>
+                <?php do_action('pdfev_template_single_footer'); ?>
+            </div>
+        </div>
+    </main>
 
     <footer class="wp-block-template-part">
         <?php block_template_part('footer'); ?>
